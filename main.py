8350210abe41e763f5ec99ba75fce461257a101c
David Bósquez <davidbd2912@gmail.com>
2023-06-28 23:24:14 -0500

    Arreglo de lineas de impresión y labels

--- main.py
+++ main.py
@@ -11,12 +11,12 @@ texto_descifrado = ""
 def Encriptar():
     texto = list(textoParaCifrar.get())
     texto_cifrado = ""
-    print(texto)
+    print("Proceso de encriptación:")
+    print("La cadena de texto a encriptar es", texto)
     for letra in texto:
         # desplazas esa letra en un numero de posiciones n(saltosCesar); si es -1 es por que es espacio blanco u otra
         # cosa fuera del alfabeto
         posicion = abecedario.find(letra)
-        print(posicion)
         if posicion == -1:
             if letra == ' ':
                 texto_cifrado += ' '
@@ -24,66 +24,69 @@ def Encriptar():
             posicion_final = posicion + int(numeroSaltos.get())
             texto_cifrado += (abecedario[posicion_final])
 
-    print(texto_cifrado)
-
     texto_cifrado = list(texto_cifrado)  # texto cifrado cesar
-    print(texto_cifrado)
+    print("La cadena encriptada es:",texto_cifrado)
     cifradoDoble = []
     for caracter in texto_cifrado:
         valorDecimalUnicode = ord(caracter)
         valorOctal = oct(valorDecimalUnicode)
         cifradoDoble.append(valorOctal)
-    print(cifradoDoble)
 
     cifradoDoble = [valor_octal.lstrip('0o') for valor_octal in
                     cifradoDoble]  # quitandole el prefijo octal a los numeros
-    print(cifradoDoble)
     lista_octales_enteros = [int(valor) for valor in cifradoDoble]  # convirtiendo los strings a enteros
-    print(lista_octales_enteros)
+    print("La cadena en el sistema octal es:", lista_octales_enteros)
 
     for i in range(len(lista_octales_enteros)):
-        print(i)
         num = lista_octales_enteros[i]
         num = num + int(numeroSaltosOctal.get())
         nuevoCifrado.append(num)
-    print(nuevoCifrado)
+    print("La cadena en el sistema octal desplazada es:", nuevoCifrado)
+
+    labelCifrado = ctk.CTkLabel(master=frame, text=str(texto_cifrado), fg_color="transparent")
+    labelCifrado.pack(pady=10, padx=10)
+
+    labelCifrado = ctk.CTkLabel(master=frame, text=str(lista_octales_enteros), fg_color="transparent")
+    labelCifrado.pack(pady=11, padx=11)
+
     labelCifrado = ctk.CTkLabel(master=frame, text=str(nuevoCifrado), fg_color="transparent")
-    labelCifrado.pack(pady=12, padx=10)
+    labelCifrado.pack(pady=13, padx=13)
 
 
 def Desencriptar():
+    print("Proceso de desencriptación:")
     lista_entero_octal = []
+    print("La cadena en el sistema octal desplazado es:", nuevoCifrado)
     for i in range(len(nuevoCifrado)):
         num = nuevoCifrado[i] - int(numeroSaltosOctal.get())
         lista_entero_octal.append(num)
-    print(lista_entero_octal)
+    print("La cadena en el sistema octal es:", lista_entero_octal)
     lista_octales = [str(valor) for valor in lista_entero_octal]
     descifradoDoble = ['0o' + valor_octal for valor_octal in lista_octales]
-    print(descifradoDoble)
 
     descifradoOctal = []
     for octales in descifradoDoble:
         valorOctal = int(octales, 8)
         caracter = chr(valorOctal)
         descifradoOctal.append(caracter)
-    print(descifradoOctal)
+    print("La cadena desencriptada de Octal a Cesar es:",descifradoOctal)
 
     texto_descifrado = ""
     for letra in descifradoOctal:
         # desplazas esa letra en un numero de posiciones n(saltosCesar); si es -1 es por que es espacio blanco u otra
         # cosa fuera del alfabeto
         posicion = abecedario.find(letra)
-        print(posicion)
+        #print(posicion)
         if posicion == -1:
             if letra == ' ':
                 texto_descifrado += ' '
         else:
             posicion_final = posicion - int(numeroSaltos.get())
             texto_descifrado += (abecedario[posicion_final])
-    print(texto_descifrado)
+    print("La cadena de texto descifrada es:",texto_descifrado)
 
     labelDescifrado = ctk.CTkLabel(master=frame, text=texto_descifrado, fg_color="transparent")
-    labelDescifrado.pack(pady=12, padx=10)
+    labelDescifrado.pack(pady=14, padx=14)
     nuevoCifrado.clear()
 
 
@@ -111,6 +114,4 @@ botonE.pack(pady=12, padx=10)
 botonD = ctk.CTkButton(master=frame, text="Desencriptar frase", command=Desencriptar)
 botonD.pack(pady=12, padx=10)
 
-print(nuevoCifrado)
-print(texto_descifrado)
 root.mainloop()
